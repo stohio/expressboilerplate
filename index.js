27e426f50ce53dfe007e56de1f4bf7f1cf3ab267
PascalRascal <marchetti.14@osu.edu>
2016-12-04 13:53:25 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = require('./server/routes');
+
+
+module.exports = function(app){
+    app.core = {};
+    app.core.dirname = __dirname;
+    //Set routes for the HTML you want to server
+    app.use('/template', express.static(__dirname + '/public/assets'));
+    //Initialize the routes
+    router(app);
+    
+    console.log("Palace Module Initialized!");
+}
